26b7fab39467a60a380863fa71f3c96699aecd60
omchaudhary28 <omchaudhary2111@gmail.com>
2025-12-21 13:22:04 +0530

    Fix auth persistence, login redirect, contact form logic

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } => "react";
+import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import { useAuth } from "../context/AuthContext";
